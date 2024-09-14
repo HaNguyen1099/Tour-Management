8835b6ea5f782df5457b6b8bf4d6b8d88e766c2b
Ha Nguyen Ngoc <nnha.1099@gmail.com>
2024-09-14 21:15:07 +0700

    Tạo route

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
-import express, { Express, Request, Response } from "express"
+import express, { Express } from "express"
 import dotenv from "dotenv"
-import sequelize from "./config/database"
-import Tour from "./models/tour.model"
+// import sequelize from "./config/database"
+import clientRoutes from "./routes/client/index.route"
 
 dotenv.config()
-sequelize
+// sequelize
 
 const app: Express = express()
 const port: number | string = process.env.PORT || 3000
@@ -12,17 +12,8 @@ const port: number | string = process.env.PORT || 3000
 app.set("views", "./views")
 app.set("view engine", "pug")
 
-app.get("/tours", async (req: Request, res: Response) => {
-    const tours = await Tour.findAll({
-        raw: true
-    })
-
-    console.log(tours)
-
-    res.render("client/pages/tours/index", {
-        tours: tours
-    })
-})
+// Client Routes 
+clientRoutes(app)
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
